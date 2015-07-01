dbc50e6f7cbd0e6986172e1e3665a7559f83c68e
Chirayu Desai <chirayudesai1@gmail.com>
2015-07-01 22:44:37 +0530

    Declare ambient light sensor and promixity sensor features
    * Got removed accidentally during msm8916-common creation
    
    Change-Id: I6a1f65dcba4d4b2a64cdb627a1f517b59110a23b
    (cherry picked from commit f53a6ccad3d9bd6f4eba923e7f6255bebdf2cdc9)

--- device.mk
+++ device.mk
@@ -22,6 +22,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 # Screen density
