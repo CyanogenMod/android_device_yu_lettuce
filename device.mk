d17a0ec59d98ed219224611a63cb9e638c582d12
TheStrix <parthbhatia98@gmail.com>
2016-03-04 04:47:23 -0500

    lettuce: Switch to crackling QMI/radio blobs
    Change-Id: Id0f8a3235978b2aa87395a3213af6e21273a80d9

--- device.mk
+++ device.mk
@@ -96,6 +96,14 @@ PRODUCT_PACKAGES += \
     libcalmodule_common \
     sensors.msm8916
 
+# Radio
+ifeq ($(QCPATH),)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
+endif
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
