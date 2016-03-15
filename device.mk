edcca449999fe27ab48c15aeb51ec175a77d5dc3
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-03-15 17:18:51 +0200

    lettuce: Remove duplicate compass permission
    * Already in handheld_core_hardware
    
    Change-Id: Ib7fd305b6e1412249521e174c3b3cd430db74b07

--- device.mk
+++ device.mk
@@ -20,7 +20,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
