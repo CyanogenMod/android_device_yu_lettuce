0456213db9c81b498c751c5bd3ebf4cdd73dfe90
Meninblack007 <sanyam.53jain@gmail.com>
2016-06-05 20:07:51 +0530

    lettuce: Remove unused prop
    * We can disable mobiledata from Setup Wizard
    
    Change-Id: I45c3458f58c386e6f3edb617885273c7637c1083
    Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -5,7 +5,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.no_cons_man_roam=1 \
     ril.ecclist=000,08,100,101,102,110,112,118,119,120,122,911,999 \
     rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
-    ro.com.android.mobiledata=false \
     ro.config.always_show_roaming=true \
     ro.telephony.default_network=9,9
 
